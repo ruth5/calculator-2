b1472ec4681a798a2133dcdba340f9b56ee79116
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-07-21 03:29:22 +0000

    implemented square, cube, pow, and mod

--- calculator.py
+++ calculator.py
@@ -5,13 +5,22 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 print("Welcome to the calculator! Please enter an operand followed by the numbers -this is a prefix calculator!")
 while True:
+
     calculator_input = (input("> "))
     equation_list = calculator_input.split(' ')
     if equation_list[0] == 'q':
         break
-    equation_list[1] = int(equation_list[1]) 
-    equation_list[2] = int(equation_list[2]) 
-    elif equation_list[0] == '+':
+    if len(equation_list)<2:
+            print("input numbers are not enough")
+    if len(equation_list)>1:
+        equation_list[1] = int(equation_list[1]) 
+    if len(equation_list)>2:
+        equation_list[2] = int(equation_list[2]) 
+    
+    num1 = equation_list[1]
+    num2 = equation_list[2]
+
+    if equation_list[0] == '+':
         print(add(equation_list[1], equation_list[2]))
     elif equation_list[0] == '-':
         print(subtract(equation_list[1], equation_list[2]))
@@ -19,6 +28,16 @@ while True:
         print(multiply(equation_list[1], equation_list[2]))
     elif equation_list[0] == '/':
         print(divide(equation_list[1], equation_list[2]))
+    elif equation_list[0] == 'square':
+        print(square(equation_list[1]))
+    elif equation_list[0] == 'cube':
+        print(cube(equation_list[1]))
+    elif equation_list[0] == 'pow':
+        print(power(num1, num2))
+    elif equation_list[0] == 'mod':
+        print(mod(num1, num2))
+    
+    
 
 
 
